e6b154d327de17bfd8f49b5b75195fda797767a9
Simran-gorad <simrangorad@gmail.com>
2023-09-16 03:16:05 +0530

    fixing product list page

--- frontend/src/pages/InventoryPage.js
+++ frontend/src/pages/InventoryPage.js
@@ -1,50 +1,60 @@
-import { Helmet } from 'react-helmet-async';
-import { useState } from 'react';
-// @mui
-import { Container, Stack, Typography } from '@mui/material';
-// components
-import { ProductSort, ProductList, ProductCartWidget, ProductFilterSidebar } from '../sections/@dashboard/products';
-// mock
-import PRODUCTS from '../_mock/products';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { Card, CardContent, CardHeader, CardMedia, Grid, Typography } from '@mui/material';
+const token = localStorage.getItem('token');
 
-// ----------------------------------------------------------------------
+function InventoryPage() {
+  const [items, setItems] = useState([]);
 
-export default function InventoryPage() {
-  const [openFilter, setOpenFilter] = useState(false);
+  useEffect(() => {
+    // Define your API endpoint URL
+    const apiUrl = 'http://192.168.3.231:4000/item/get-allItems';
+    const headers = {
+      Authorization: `Bearer ${token}`,
+    };
 
-  const handleOpenFilter = () => {
-    setOpenFilter(true);
-  };
-
-  const handleCloseFilter = () => {
-    setOpenFilter(false);
-  };
+    // Fetch data from the API using Axios
+    axios.get(apiUrl, { headers })
+      .then((response) => {
+        // Set the retrieved data to the "items" state
+        setItems(response.data);
+      })
+      .catch((error) => {
+        console.error('Error fetching data:', error);
+      });
+  }, []);
 
   return (
-    <>
-      <Helmet>
-        <title> Dashboard: Products | CodeNando </title>
-      </Helmet>
-
-      <Container>
-        <Typography variant="h4" sx={{ mb: 5 }}>
-          Inventory products
-        </Typography>
-
-        <Stack direction="row" flexWrap="wrap-reverse" alignItems="center" justifyContent="flex-end" sx={{ mb: 5 }}>
-          <Stack direction="row" spacing={1} flexShrink={0} sx={{ my: 1 }}>
-            <ProductFilterSidebar
-              openFilter={openFilter}
-              onOpenFilter={handleOpenFilter}
-              onCloseFilter={handleCloseFilter}
-            />
-            <ProductSort />
-          </Stack>
-        </Stack>
-
-        <ProductList products={PRODUCTS} />
-        <ProductCartWidget />
-      </Container>
-    </>
+    <div>
+      <h1>List of Items</h1>
+      <Grid container spacing={2}>
+        {items.map((item) => (
+          <Grid item xs={12} sm={6} md={4} lg={3} key={item._id}>
+            <Card>
+              <CardHeader
+                title={item.name}
+                subheader={`Category: ${item.category}`}
+              />
+              <CardMedia
+                component="img"
+                height="200"
+                image="https://via.placeholder.com/200" // Replace with your actual image URL
+                alt={item.name}
+              />
+              <CardContent>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Location: {item.location}
+                </Typography>
+                <Typography variant="body2" color="textSecondary" component="p">
+                  Status: {item.status}
+                </Typography>
+              </CardContent>
+            </Card>
+          </Grid>
+        ))}
+      </Grid>
+    </div>
   );
 }
+
+export default InventoryPage;
